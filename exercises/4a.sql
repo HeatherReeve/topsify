aeae9843665ec954d3fa4ffb18e60cd49a9b5bc2
heather Reeve <heather.reeve@scope.org.uk>
2024-07-12 13:23:45 +0100

    4a completed

--- exercises/4a.sql
+++ exercises/4a.sql
@@ -1,3 +1,5 @@
 -- List the name of every album in the database
 -- Include the artist name
 -- (Make sure to select the album name first, then the artist name)
+SELECT albums.name, artists.name FROM albums
+JOIN artists ON artists.id = albums.artist_id;
--- exercises/4b.sql
+++ exercises/4b.sql
@@ -1,3 +1,6 @@
 -- List the 10 slowest tracks in the database
 -- Order by tempo, with the slowest first
 -- Include the track id, track title and track tempo
+SELECT tracks.id, tracks.name, features.tempo 
+FROM tracks
+JOIN features ON features.tempo = features.tracks_id
