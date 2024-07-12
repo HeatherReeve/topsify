ab3e83e4e296c2ce869df451ce878e3ba67c663f
heather Reeve <heather.reeve@scope.org.uk>
2024-07-12 13:57:06 +0100

    5a completed

--- exercises/4b.sql
+++ exercises/4b.sql
@@ -1,6 +1,8 @@
 -- List the 10 slowest tracks in the database
 -- Order by tempo, with the slowest first
 -- Include the track id, track title and track tempo
-SELECT tracks.id, tracks.name, features.tempo 
-FROM tracks
-JOIN features ON features.tempo = features.tracks_id
+SELECT features.track_id, tracks.name, features.tempo 
+FROM features
+JOIN tracks ON features.track_id = tracks.id
+ORDER BY tempo
+LIMIT 10;
